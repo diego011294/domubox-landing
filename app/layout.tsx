31b05d17528da037f2a60829e953a4602b90ea70
diego <diego0112@hotmail.com>
2026-02-21 13:07:44 +0100

    Meta pixel implementado

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import Navbar from "@/components/navbar/Navbar";
 import Footer from "@/components/footer/Footer";
 import MaintenancePage from "./maintenance/page";
 import { Playfair_Display, DM_Sans, Lexend } from "next/font/google";
+import Script from "next/script";
 
 const playfair = Playfair_Display({
   subsets: ["latin"],
@@ -51,6 +52,43 @@ export default function RootLayout({
         <link rel="icon" href="/favicon.ico" type="image/x-icon" />
         <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
         <link rel="apple-touch-icon" href="/favicon.png" />
+
+      {/* Meta Pixel */}
+      <Script
+        id="facebook-pixel"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;
+            n.push=n;
+            n.loaded=!0;
+            n.version='2.0';
+            n.queue=[];
+            t=b.createElement(e);
+            t.async=!0;
+            t.src=v;
+            s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)
+            }(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '894013069888151');
+            fbq('track', 'PageView');
+          `,
+        }}
+      />
+
+      <noscript>
+        <img
+          height="1"
+          width="1"
+          style={{ display: "none" }}
+          src="https://www.facebook.com/tr?id=894013069888151&ev=PageView&noscript=1"
+        />
+      </noscript>
+
       </head>
       <body className="bg-white">
         {maintenance ? (
